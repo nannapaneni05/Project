5b89eb5bbae88806373b41070b0fc9b4396223ef
kuldeep <kuldeep.r@affinity.com>
2017-04-04 02:20:51 +0530

    update single wall select and delete

--- js/originScale.js
+++ js/originScale.js
@@ -34,6 +34,7 @@ function setNewOrigin (intersects ) {
         }
     });
 
+    /*
     var polys =  [].concat(floor.gridData.polys);
     polys.forEach(function (poly, index, list) {
         poly.cubes.forEach(function (cube, index, list) {
@@ -47,6 +48,7 @@ function setNewOrigin (intersects ) {
         redrawLine();
         floor.gridData.polys[index].line = _tempLine;
     });
+    */
     createPlane();
 }
 
