ac788368bd837bc40d55d3870bd57a93110589f0
kuldeep <kuldeep.r@affinity.com>
2017-04-10 20:20:25 +0530

    udpate

--- js/index.js
+++ js/index.js
@@ -112,6 +112,10 @@ function bindListeners () {
     document.onkeydown = onDocumentKeyDown;
 
     $('a.subMenuButton').click(function() {
+        if(  $( this).not( ".selectWalls" ) ) {
+            removeSelectWallBox();
+        }   
+
         container.style.cursor = "default";
         $(this).siblings().removeClass('active');
         $('.toolbox-tools').attr('hidden', true);
